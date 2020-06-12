3cc9075e6b7747386b0c156743926db0b0df7413
Adithya Madhusoodanan <44079850+adithyamadhusoodanan@users.noreply.github.com>
2020-06-12 20:07:45 +0530

    Create Openclass.java

---
+++ Openclass.java
@@ -0,0 +1,9 @@
+package com.example.itminorproject;
+
+public class Openclass {
+
+    public static String r;
+    public static String s;
+    public static String e;
+    public static String link;
+}
