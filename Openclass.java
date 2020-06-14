c6b4865a0e60b2bbdc21b63b76e7e1dcf5d1fdc9
Adithya Madhusoodanan <44079850+adithyamadhusoodanan@users.noreply.github.com>
2020-06-15 01:20:07 +0530

    Update and rename Openclass.java to singleToneClass.java

--- Openclass.java
+++
@@ -1,9 +0,0 @@
-package com.example.itminorproject;
-
-public class Openclass {
-
-    public static String r;
-    public static String s;
-    public static String e;
-    public static String link;
-}
